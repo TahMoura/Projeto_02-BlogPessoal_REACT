ad4e3acb1bd9cee42df6874c40b29a6a6eec464c
TahMoura <talia.git@outlook.com>
2024-01-26 19:22:59 -0300

    Atualiando o projeto blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import Home from './paginas/home/Home';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+
+import Login from './pages/Login/Login'
+import Cadastro from './pages/Cadastro/Cadastro'
+import Home from './pages/Home/Home'
+import ListaTemas from './components/temas/listaTemas/ListaTemas'
+import FormularioTema from './components/temas/formularioTema/FormularioTema'
+import DeletarTema from './components/temas/deletarTemas/DeletarTema'
 
 function App() {
-  return (
-   <Home />
-  );
+    return (
+        <AuthProvider>  {/* Compartilha com a Aplicação todos os dados do Contexto */}
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>  {/* Define o tamanho minimo das páginas */}
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/cadastro" element={<Cadastro />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/temas" element={<ListaTemas />} />
+                        <Route path="/cadastroTema" element={<FormularioTema />} />
+                        <Route path="/editarTema/:id" element={<FormularioTema />} />   {/* :id = é uma variavel que vem pela url do Front, que represenda o id do item que vai ser editado */}
+                        <Route path="/deletarTema/:id" element={<DeletarTema />} />     {/* :id = é uma variavel que vem pela url do Front, que represenda o id do item que vai ser excluído */}
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </ AuthProvider>
+    )
 }
 
-export default App;
+export default App
